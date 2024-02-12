065399b99df3270d61f34c4665481731111fc765
Thainguyenluong <Thainguyenluong@github.com>
2024-02-11 22:59:05 -0500

    add class

---
+++ Diymodel.py
@@ -0,0 +1,8 @@
+import os
+class Diymodel:
+    def _init_(self,idnumber):
+        self.idnumber = idnumber
+    def upload(filename):
+        fn = os.path.basename(filename)
+        open(fn,'wb').write(filename.file.read())
+
