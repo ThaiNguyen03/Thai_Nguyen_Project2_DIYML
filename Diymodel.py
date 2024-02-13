763e8ec515693c22d6785d7fe42403ed7c9e16c0
Thainguyenluong <Thainguyenluong@github.com>
2024-02-12 21:54:00 -0500

    remove file

--- Diymodel.py
+++
@@ -1,8 +0,0 @@
-import os
-class Diymodel:
-    def _init_(self,idnumber):
-        self.idnumber = idnumber
-    def upload(filename):
-        fn = os.path.basename(filename)
-        open(fn,'wb').write(filename.file.read())
-
