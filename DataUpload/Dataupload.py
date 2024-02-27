912abf1b0a017d0320c1d974cdaa47a740954f36
Thainguyenluong <Thainguyenluong@github.com>
2024-02-27 17:00:06 -0500

    fix dataupload module

--- DataUpload/Dataupload.py
+++ DataUpload/Dataupload.py
@@ -1,24 +1,24 @@
-from flask import *
-from distutils.log import debug
-from fileinput import filename
+from flask import Flask, request
 import os
 import pandas as pd
+
 app = Flask(__name__)
-@app.route("/")
-@app.route('/upload_images', methods=['POST'])
+
+@app.route('/upload_images/<user_id>/<project_id>', methods=['POST'])
 def upload_images(user_id, project_id):
+    if 'file' not in request.files:
+        return 'No file part', 400
     f = request.files['file']
     f.save(f.filename)
+    return 'Image uploaded successfully', 200
 
-    return 'Image uploaded successfully'
-
-@app.route('/upload_label/', methods=['POST'])
+@app.route('/upload_label/<user_id>/<project_id>', methods=['POST'])
 def upload_label(user_id, project_id):
+    if 'file' not in request.files:
+        return 'No file part', 400
     f = request.files['file']
     f.save(f.filename)
-    return 'Label uploaded successfully'
+    return 'Label uploaded successfully', 200
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
