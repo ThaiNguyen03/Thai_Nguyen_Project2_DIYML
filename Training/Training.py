cae0ce94c92756cb58d85c3439d296e47ff2161c
Thainguyenluong <Thainguyenluong@github.com>
2024-03-23 17:06:57 -0400

    backup

--- Training/Training.py
+++ Training/Training.py
@@ -1,60 +1,119 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from pymongo import MongoClient
-import json,os
-from transformers import Trainer, TrainingArguments, AutoModelForSequenceClassification, AutoTokenizer
+
+import torch
+from datasets import load_from_disk, load_dataset
+import json, os
+from transformers import Trainer, DefaultDataCollator, TrainingArguments, AutoModelForImageClassification, AutoTokenizer, \
+    AutoFeatureExtractor
+
 app = Flask(__name__)
 api = Api(app)
+
 # Initialize MongoDB client
 mongo_url = 'mongodb://localhost:27017'
 client = MongoClient(mongo_url)
 db = client['ML_data']
-model_collection = db['parameters']  # Collection to store image metadata
+model_collection = db['model_data']  # Collection to store model parameters
+stats_collection = db['stats']  # Collection to store training stats
+
+
+class UploadParameters(Resource):
+    def post(self):
+        data = request.get_json()
+        user_id = data.get('user_id')
+        project_id = data.get('project_id')
+        parameters = data.get('parameters')
+        model_collection.insert_one({
+            'user_id': user_id,
+            'project_id': project_id,
+            'parameters': parameters
+        })
+        return {"message": "Parameters uploaded successfully"}, 200
+
 
 class StartTraining(Resource):
     def post(self):
         data = request.get_json()
-        model_name = data.get('model')
+        user_id = data.get('user_id')
         project_id = data.get('project_id')
-        parameters = model_collection.find_one({"project_id": project_id})
+        model_name = data.get('model_name')
+        num_labels = data.get('num_labels')
+        labels = data.get('labels')
+        train_dataset_path = data.get('train_dataset')
+        parameters_data = model_collection.find_one({"user_id": user_id, "project_id": project_id})
+        parameters = parameters_data['parameters']
         # load model and tokenizer
-        model = AutoModelForSequenceClassification.from_pretrained(model_name)
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        train_dataset = ...
-        eval_dataset = ...
+
+        # tokenizer = AutoTokenizer.from_pretrained(model_name, num_labels)
+
+        # load datasets
+        # train_dataset = load_from_disk(f'{user_id}/{project_id}/images/image_train')
+        # eval_dataset = load_from_disk(f'{user_id}/{project_id}/images/image_eval')
+        api_dataset = load_from_disk(train_dataset_path)
+        api_dataset = api_dataset.train_test_split(test_size=0.2)
+        labels = api_dataset["train"].features["labels"].names
+        feature_extractor = AutoFeatureExtractor.from_pretrained(model_name)
+        label2id, id2label = dict(), dict()
+        for i, label in enumerate(labels):
+            label2id[label] = str(i)
+            id2label[str(i)] = label
+        model = AutoModelForImageClassification.from_pretrained(model_name,
+                                                                num_labels=len(labels),
+                                                                id2label=id2label,
+                                                                label2id=label2id
+                                                                )
+        # eval_dataset = load_dataset('parquet', data_files='/home/thai/training_test/validation-00000-of-00003.parquet')
         # load training arguments
         training_args = TrainingArguments(
-            output_dir='./results',
+            output_dir=f'./results/{user_id}/{project_id}',
             num_train_epochs=parameters.get('num_train_epochs'),
+            evaluation_strategy="steps",
             per_device_train_batch_size=parameters.get('per_device_train_batch_size'),
-            per_device_eval_batch_size=parameters.get('per_device_eval_batch_size'),
             warmup_steps=parameters.get('warmup_steps'),
             weight_decay=parameters.get('weight_decay'),
-            logging_dir='./logs',
+            logging_dir=f'./logs/{user_id}/{project_id}',
+            remove_unused_columns=False,
         )
+        data_collator = DefaultDataCollator()
         trainer = Trainer(
             model=model,
             args=training_args,
-            train_dataset=train_dataset,
-            eval_dataset=eval_dataset
+            data_collator=data_collator,
+            train_dataset=api_dataset["train"],
+            eval_dataset=api_dataset["test"],
+            tokenizer=feature_extractor
         )
+
         trainer.train()
 
-        return {"message": model_name}, 200
+        # Save training stats to the database
+        stats_collection.insert_one({
+            'user_id': user_id,
+            'project_id': project_id,
+            'model_name': model_name,
+            'training_stats': trainer.evaluate()
+        })
+
+        return {"message": f"Training for model {model_name} completed successfully"}, 200
+
 
 class GetTrainingStats(Resource):
-    def get(self):
-        data = request.get_json()
-        model_name = data.get('model')
-        output_dir = './results'
-        training_stats_file = os.path.join(output_dir, 'train_results.txt')
-        if os.path.exists(training_stats_file):
-            with open(training_stats_file, 'r') as f:
-                training_stats = json.load(f)
-        else:
-            training_stats = "No training stats found"
-        return model_name, 200
+    def get(self, user_id, project_id, model_name):
+        stats = stats_collection.find_one({
+            'user_id': user_id,
+            'project_id': project_id,
+            'model_name': model_name
+        })
 
+        if stats:
+            return stats['training_stats'], 200
+        else:
+            return {"message": "No training stats found"}, 404
+
+
+api.add_resource(UploadParameters, '/upload_parameters')
 api.add_resource(StartTraining, '/start_training')
 api.add_resource(GetTrainingStats, '/get_training_stats')
 
--- Training/TrainingTest.py
+++ Training/TrainingTest.py
@@ -1,6 +1,22 @@
 import pytest
-from flask import json
-from Training import app
+import logging
+import tracemalloc
+from unittest.mock import patch, MagicMock
+
+from datasets import load_dataset
+
+from Training import app, StartTraining, GetTrainingStats
+
+logging.basicConfig(filename='./testTraining.log', filemode='a', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+mylogger = logging.getLogger()
+fhandler = logging.FileHandler(filename='testDataUpload.log', mode='a')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fhandler.setFormatter(formatter)
+mylogger.addHandler(fhandler)
+mylogger.setLevel(logging.DEBUG)
+tracemalloc.start()
+
 
 @pytest.fixture
 def client():
@@ -8,14 +24,38 @@ def client():
     with app.test_client() as client:
         yield client
 
+
+def test_upload_parameters(client):
+    datasets = load_dataset("food101", split="train[:5000]")
+    datasets.save_to_disk('/home/thai/training_test/')
+    rv = client.post('/upload_parameters', json={
+        'user_id': 'test_user',
+        'project_id': 'test_project',
+        'parameters': {'num_train_epochs': 3, 'per_device_train_batch_size': 16, 'per_device_eval_batch_size': 64,
+                       'warmup_steps': 500, 'weight_decay': 0.01},
+        'train_dataset': '/home/thai/training_test/'
+    })
+    assert b'Parameters uploaded successfully' in rv.data
+
+
+
 def test_start_training(client):
-    # Test the StartTraining endpoint
-    response = client.post('/start_training', json={'model': 'test_model', 'project_id': 'test_project_id'})
-    assert response.status_code == 200
-    assert response.get_json() == {"message": "test_model"}
+
+
+    rv = client.post('/start_training', json={
+        'user_id': 'test_user',
+        'project_id': 'test_project',
+        'model_name': 'google/vit-base-patch16-224-in21k'
+    })
+    assert b'Training for model test_model completed successfully' in rv.data
+
 
 def test_get_training_stats(client):
-    # Test the GetTrainingStats endpoint
-    response = client.get('/get_training_stats', json={'model': 'test_model'})
-    assert response.status_code == 200
-    assert response.get_json() == "test_model"
+    rv = client.get('/get_training_stats/test_user/test_project/test_model')
+    assert rv.status_code == 200
+    current, peak = tracemalloc.get_traced_memory()
+    mylogger.info(f"Current memory usage is {current / 10 ** 6}MB; Peak was {peak / 10 ** 6}MB")
+    tracemalloc.stop()
+
+# if __name__ == "__main__":
+#   pytest.main()
