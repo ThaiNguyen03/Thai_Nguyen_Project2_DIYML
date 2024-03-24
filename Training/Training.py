1cda6e02dbd818449f1a5789c93fc3c387f3fb7a
ThaiNguyen03 <quocthai@bu.edu>
2024-03-24 18:08:37 -0400

    add code

--- Training/Training.py
+++ Training/Training.py
@@ -13,7 +13,9 @@ from transformers import Trainer, DefaultDataCollator, TrainingArguments, AutoMo
     AutoTokenizer, \
     AutoFeatureExtractor, AutoImageProcessor
 from queue import Queue
-from threading import Thread
+from threading import Thread, Event
+
+task_complete_event = Event()
 app = Flask(__name__)
 api = Api(app)
 
@@ -24,6 +26,7 @@ db = client['ML_data']
 model_collection = db['model_data']  # Collection to store model parameters
 stats_collection = db['stats']  # Collection to store training stats
 task_queue = Queue()
+
 
 class UploadParameters(Resource):
     def post(self):
@@ -40,107 +43,124 @@ class UploadParameters(Resource):
         return {"message": "Parameters uploaded successfully"}, 200
 
 
-
 def start_training(data):
-        data = request.get_json()
-        user_id = data.get('user_id')
-        project_id = data.get('project_id')
-        model_name = data.get('model_name')
-        num_labels = data.get('num_labels')
-        labels = data.get('labels')
-        train_dataset_path = data.get('train_dataset')
-        parameters_data = model_collection.find_one({"user_id": user_id, "project_id": project_id})
-        parameters = parameters_data['parameters']
-       # api_dataset = load_from_disk(train_dataset_path)
-        api_dataset= load_dataset('parquet',data_files='./training_test/training_test.parquet')
+    user_id = data.get('user_id')
+    project_id = data.get('project_id')
+    model_name = data.get('model_name')
+    train_dataset_path = data.get('train_dataset')
+    parameters_data = model_collection.find_one({"user_id": user_id, "project_id": project_id})
+    parameters = parameters_data['parameters']
+   # train_dataset_path = parameters['train_dataset']
+    # api_dataset = load_from_disk(train_dataset_path)
+    api_dataset = load_dataset('parquet', data_files= train_dataset_path)
 
-        api_dataset = api_dataset["train"].train_test_split(test_size=0.2)
-        # Check if 'labels' is in features
-       # if 'labels' in api_dataset["train"].features:
-        labels = api_dataset["train"].features["label"].names
-        #else:
-         #   print("The 'labels' feature does not exist in the dataset. Please check the feature names.")
-         #   print(api_dataset["train"].features.keys())
+    api_dataset = api_dataset["train"].train_test_split(test_size=0.2)
+    # Check if 'labels' is in features
+    # if 'labels' in api_dataset["train"].features:
+    labels = api_dataset["train"].features["label"].names
+    # else:
+    #   print("The 'labels' feature does not exist in the dataset. Please check the feature names.")
+    #   print(api_dataset["train"].features.keys())
 
-        #labels = api_dataset["train"].features["labels"].names
-        feature_extractor = AutoFeatureExtractor.from_pretrained(model_name)
-        label2id, id2label = dict(), dict()
-        for i, label in enumerate(labels):
-            label2id[label] = str(i)
-            id2label[str(i)] = label
+    # labels = api_dataset["train"].features["labels"].names
+    feature_extractor = AutoFeatureExtractor.from_pretrained(model_name)
+    label2id, id2label = dict(), dict()
+    for i, label in enumerate(labels):
+        label2id[label] = str(i)
+        id2label[str(i)] = label
 
-        checkpoint = model_name
-        image_processor = AutoImageProcessor.from_pretrained(checkpoint)
+    checkpoint = model_name
+    image_processor = AutoImageProcessor.from_pretrained(checkpoint)
 
-        normalize = Normalize(mean=image_processor.image_mean, std=image_processor.image_std)
-        size = (
-            image_processor.size["shortest_edge"]
-            if "shortest_edge" in image_processor.size
-            else (image_processor.size["height"], image_processor.size["width"])
-        )
-        _transforms = Compose([RandomResizedCrop(size), ToTensor(), normalize])
+    normalize = Normalize(mean=image_processor.image_mean, std=image_processor.image_std)
+    size = (
+        image_processor.size["shortest_edge"]
+        if "shortest_edge" in image_processor.size
+        else (image_processor.size["height"], image_processor.size["width"])
+    )
+    _transforms = Compose([RandomResizedCrop(size), ToTensor(), normalize])
 
-        def transforms(examples):
-            examples["pixel_values"] = [_transforms(img.convert("RGB")) for img in examples["image"]]
-            del examples["image"]
-            return examples
+    def transforms(examples):
+        examples["pixel_values"] = [_transforms(img.convert("RGB")) for img in examples["image"]]
+        del examples["image"]
+        return examples
 
-        api_dataset = api_dataset.with_transform(transforms)
-        model = AutoModelForImageClassification.from_pretrained(model_name,
-                                                                num_labels=len(labels),
-                                                                id2label=id2label,
-                                                                label2id=label2id
-                                                                )
-        accuracy = evaluate.load("accuracy")
+    api_dataset = api_dataset.with_transform(transforms)
+    model = AutoModelForImageClassification.from_pretrained(model_name,
+                                                            num_labels=len(labels),
+                                                            id2label=id2label,
+                                                            label2id=label2id
+                                                            )
+    accuracy = evaluate.load("accuracy")
 
-        def compute_metrics(eval_pred):
-            predictions, labels = eval_pred
-            predictions = np.argmax(predictions, axis=1)
-            return accuracy.compute(predictions=predictions, references=labels)
+    def compute_metrics(eval_pred):
+        predictions, labels = eval_pred
+        predictions = np.argmax(predictions, axis=1)
+        return accuracy.compute(predictions=predictions, references=labels)
 
-        # eval_dataset = load_dataset('parquet', data_files='/home/thai/training_test/validation-00000-of-00003.parquet')
-        # load training arguments
-        training_args = TrainingArguments(
-            output_dir=f'./results/{user_id}/{project_id}',
-            num_train_epochs=parameters.get('num_train_epochs'),
-            evaluation_strategy="epoch",
-            save_strategy= "epoch",
-            learning_rate=parameters.get('learning_rate'),
-            per_device_train_batch_size=parameters.get('per_device_train_batch_size'),
-            per_device_eval_batch_size=parameters.get('per_device_eval_batch_size'),
-            gradient_accumulation_steps=parameters.get('gradient_accumulation_steps'),
-            warmup_ratio=parameters.get('warmup_ratio'),
-            logging_steps=parameters.get('logging_steps'),
-            load_best_model_at_end=True,
-            logging_dir=f'./logs/{user_id}/{project_id}',
-            remove_unused_columns=False,
-        )
-        data_collator = DefaultDataCollator()
-        trainer = Trainer(
-            model=model,
-            args=training_args,
-            data_collator=data_collator,
-            train_dataset=api_dataset["train"],
-            eval_dataset=api_dataset["test"],
-            tokenizer=feature_extractor
-        )
-
+    # eval_dataset = load_dataset('parquet', data_files='/home/thai/training_test/validation-00000-of-00003.parquet')
+    # load training arguments
+    training_args = TrainingArguments(
+        output_dir=f'./results/{user_id}/{project_id}',
+        num_train_epochs=parameters.get('num_train_epochs'),
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        learning_rate=parameters.get('learning_rate'),
+        per_device_train_batch_size=parameters.get('per_device_train_batch_size'),
+        per_device_eval_batch_size=parameters.get('per_device_eval_batch_size'),
+        gradient_accumulation_steps=parameters.get('gradient_accumulation_steps'),
+        warmup_ratio=parameters.get('warmup_ratio'),
+        logging_steps=parameters.get('logging_steps'),
+        load_best_model_at_end=True,
+        logging_dir=f'./logs/{user_id}/{project_id}',
+        remove_unused_columns=False,
+    )
+    data_collator = DefaultDataCollator()
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=api_dataset["train"],
+        eval_dataset=api_dataset["test"],
+        tokenizer=feature_extractor
+    )
+    try:
         trainer.train()
 
-        # Save training stats to the database
-        stats_collection.insert_one({
-            'user_id': user_id,
-            'project_id': project_id,
-            'model_name': model_name,
-            'training_stats': trainer.evaluate()
-        })
+    except Exception as e:
+        return {"message": "Training failed"}, 400
 
-        return {"message": f"Training for model {model_name} completed successfully"}, 200
+    # Save training stats to the database
+    stats_collection.insert_one({
+        'user_id': user_id,
+        'project_id': project_id,
+        'model_name': model_name,
+        'training_stats': trainer.evaluate()
+    })
+
+    return {"message": f"Training for model {model_name} completed successfully"}, 200
+
+
+def worker():
+    while not task_queue.empty():
+        data = task_queue.get()
+        start_training(data)
+        task_queue.task_done()
+    task_complete_event.set()
+
 
 class StartTraining(Resource):
     def post(self):
         data = request.get_json()
         task_queue.put(data)
+        task_complete_event.clear()
+        worker_thread = Thread(target=worker)
+        worker_thread.start()
+
+        task_complete_event.wait()
+        return {"message": "Training request received"}, 200
+
+
 class GetTrainingStats(Resource):
     def get(self):
         data = request.get_json()
@@ -162,12 +182,6 @@ class GetTrainingStats(Resource):
 api.add_resource(UploadParameters, '/upload_parameters')
 api.add_resource(StartTraining, '/start_training')
 api.add_resource(GetTrainingStats, '/get_training_stats')
-def worker():
-    while True:
-        data = task_queue.get()
-        start_training(data)
-        task_queue.task_done()
+
 if __name__ == '__main__':
-    worker_thread = Thread(target=worker)
-    worker_thread.start()
     app.run(debug=True)
--- Training/TrainingTest.py
+++ Training/TrainingTest.py
@@ -5,7 +5,7 @@ from unittest.mock import patch, MagicMock
 
 from datasets import load_dataset
 
-from Training import app, StartTraining, GetTrainingStats
+from Training import app, StartTraining, GetTrainingStats, task_queue, task_complete_event
 
 logging.basicConfig(filename='./testTraining.log', filemode='a', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
@@ -26,47 +26,80 @@ def client():
 
 
 def test_upload_parameters(client):
-    test_set = load_dataset("food101", split="train[:100]")
-    #test_set.save_to_disk('./training_test')
-    test_set.to_parquet('./training_test/training_test.parquet')
     rv = client.post('/upload_parameters', json={
         'user_id': 'test_user',
         'project_id': 'test_project',
         'parameters': {
-                        'learning_rate': 5e-5,
-                        'per_device_train_batch_size':16,
-                        'gradient_accumulation_steps':4,
-                        'per_device_eval_batch_size':16,
-                        'num_train_epochs': 3,
-                        'warmup_ratio':0.1,
-                        'logging_steps': 10,
-    },
-        'train_dataset': './training_test/training_test.parquet'
+            'learning_rate': 5e-5,
+            'per_device_train_batch_size': 16,
+            'gradient_accumulation_steps': 4,
+            'per_device_eval_batch_size': 16,
+            'num_train_epochs': 3,
+            'warmup_ratio': 0.1,
+            'logging_steps': 10,
+        },
+
     })
     assert b'Parameters uploaded successfully' in rv.data
 
 
+# def test_start_training(client):
+#    rv = client.post('/start_training', json={
+#        'user_id': 'test_user',
+#        'project_id': 'test_project',
+#        'model_name': 'google/vit-base-patch16-224-in21k'
+#    })
+#   try:
+#      assert rv.status_code == 200
+#     mylogger.info("test_start_training passed")
+# except AssertionError:
+#    mylogger.error("test_start_training not passed")
+#    current, peak = tracemalloc.get_traced_memory()
+#    mylogger.info(f"Current memory usage is {current / 10 ** 6}MB; Peak was {peak / 10 ** 6}MB")
+#    tracemalloc.stop()
 
-def test_start_training(client):
-    rv = client.post('/start_training', json={
+
+# assert b'Training for model test_model completed successfully' in rv.data
+def test_task_queue(client):
+    test_set = load_dataset("food101", split="train[:100]")
+    # test_set.save_to_disk('./training_test')
+    test_set.to_parquet('./training_test/training_test.parquet')
+    with task_queue.mutex:
+        task_queue.queue.clear()
+    task_complete_event.clear()
+    rv1 = client.post('/start_training', json={
         'user_id': 'test_user',
         'project_id': 'test_project',
-        'model_name': 'google/vit-base-patch16-224-in21k'
+        'model_name': 'google/vit-base-patch16-224-in21k',
+        'train_dataset': './training_test/training_test.parquet'
     })
-    assert rv.status_code ==200
-   # assert b'Training for model test_model completed successfully' in rv.data
+
+    rv2 = client.post('/start_training', json={
+        'user_id': 'test_user',
+        'project_id': 'test_project',
+        'model_name': 'microsoft/beit-base-patch16-224-pt22k',
+        'train_dataset': './training_test/training_test.parquet'
+    })
+
+    task_complete_event.wait()
+    assert task_queue.empty()
+
+    assert rv1.status_code == 200
+    assert rv2.status_code == 200
 
 
 def test_get_training_stats(client):
-    rv = client.get('/get_training_stats',json = {
+    rv = client.get('/get_training_stats', json={
         'user_id': 'test_user',
         'project_id': 'test_project',
         'model_name': 'google/vit-base-patch16-224-in21k'
     })
     assert rv.status_code == 200
-    current, peak = tracemalloc.get_traced_memory()
-    mylogger.info(f"Current memory usage is {current / 10 ** 6}MB; Peak was {peak / 10 ** 6}MB")
-    tracemalloc.stop()
+
+
+current, peak = tracemalloc.get_traced_memory()
+mylogger.info(f"Current memory usage is {current / 10 ** 6}MB; Peak was {peak / 10 ** 6}MB")
+tracemalloc.stop()
 
 # if __name__ == "__main__":
 #   pytest.main()
