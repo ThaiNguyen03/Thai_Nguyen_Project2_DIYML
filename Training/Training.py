c5619350f16e4291a240398498ee2bfdedb73e41
ThaiNguyen03 <quocthai@bu.edu>
2024-03-24 00:00:04 -0400

    add working training code and unit test

--- Training/Training.py
+++ Training/Training.py
@@ -1,12 +1,17 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from pymongo import MongoClient
-
+import numpy as np
+import sklearn
+import evaluate
 import torch
+import PIL
 from datasets import load_from_disk, load_dataset
 import json, os
-from transformers import Trainer, DefaultDataCollator, TrainingArguments, AutoModelForImageClassification, AutoTokenizer, \
-    AutoFeatureExtractor
+from torchvision.transforms import RandomResizedCrop, Compose, Normalize, ToTensor
+from transformers import Trainer, DefaultDataCollator, TrainingArguments, AutoModelForImageClassification, \
+    AutoTokenizer, \
+    AutoFeatureExtractor, AutoImageProcessor
 
 app = Flask(__name__)
 api = Api(app)
@@ -44,35 +49,67 @@ class StartTraining(Resource):
         train_dataset_path = data.get('train_dataset')
         parameters_data = model_collection.find_one({"user_id": user_id, "project_id": project_id})
         parameters = parameters_data['parameters']
-        # load model and tokenizer
+       # api_dataset = load_from_disk(train_dataset_path)
+        api_dataset= load_dataset('parquet',data_files='./training_test/training_test.parquet')
 
-        # tokenizer = AutoTokenizer.from_pretrained(model_name, num_labels)
+        api_dataset = api_dataset["train"].train_test_split(test_size=0.2)
+        # Check if 'labels' is in features
+       # if 'labels' in api_dataset["train"].features:
+        labels = api_dataset["train"].features["label"].names
+        #else:
+         #   print("The 'labels' feature does not exist in the dataset. Please check the feature names.")
+         #   print(api_dataset["train"].features.keys())
 
-        # load datasets
-        # train_dataset = load_from_disk(f'{user_id}/{project_id}/images/image_train')
-        # eval_dataset = load_from_disk(f'{user_id}/{project_id}/images/image_eval')
-        api_dataset = load_from_disk(train_dataset_path)
-        api_dataset = api_dataset.train_test_split(test_size=0.2)
-        labels = api_dataset["train"].features["labels"].names
+        #labels = api_dataset["train"].features["labels"].names
         feature_extractor = AutoFeatureExtractor.from_pretrained(model_name)
         label2id, id2label = dict(), dict()
         for i, label in enumerate(labels):
             label2id[label] = str(i)
             id2label[str(i)] = label
+
+        checkpoint = model_name
+        image_processor = AutoImageProcessor.from_pretrained(checkpoint)
+
+        normalize = Normalize(mean=image_processor.image_mean, std=image_processor.image_std)
+        size = (
+            image_processor.size["shortest_edge"]
+            if "shortest_edge" in image_processor.size
+            else (image_processor.size["height"], image_processor.size["width"])
+        )
+        _transforms = Compose([RandomResizedCrop(size), ToTensor(), normalize])
+
+        def transforms(examples):
+            examples["pixel_values"] = [_transforms(img.convert("RGB")) for img in examples["image"]]
+            del examples["image"]
+            return examples
+
+        api_dataset = api_dataset.with_transform(transforms)
         model = AutoModelForImageClassification.from_pretrained(model_name,
                                                                 num_labels=len(labels),
                                                                 id2label=id2label,
                                                                 label2id=label2id
                                                                 )
+        accuracy = evaluate.load("accuracy")
+
+        def compute_metrics(eval_pred):
+            predictions, labels = eval_pred
+            predictions = np.argmax(predictions, axis=1)
+            return accuracy.compute(predictions=predictions, references=labels)
+
         # eval_dataset = load_dataset('parquet', data_files='/home/thai/training_test/validation-00000-of-00003.parquet')
         # load training arguments
         training_args = TrainingArguments(
             output_dir=f'./results/{user_id}/{project_id}',
             num_train_epochs=parameters.get('num_train_epochs'),
-            evaluation_strategy="steps",
+            evaluation_strategy="epoch",
+            save_strategy= "epoch",
+            learning_rate=parameters.get('learning_rate'),
             per_device_train_batch_size=parameters.get('per_device_train_batch_size'),
-            warmup_steps=parameters.get('warmup_steps'),
-            weight_decay=parameters.get('weight_decay'),
+            per_device_eval_batch_size=parameters.get('per_device_eval_batch_size'),
+            gradient_accumulation_steps=parameters.get('gradient_accumulation_steps'),
+            warmup_ratio=parameters.get('warmup_ratio'),
+            logging_steps=parameters.get('logging_steps'),
+            load_best_model_at_end=True,
             logging_dir=f'./logs/{user_id}/{project_id}',
             remove_unused_columns=False,
         )
@@ -100,7 +137,11 @@ class StartTraining(Resource):
 
 
 class GetTrainingStats(Resource):
-    def get(self, user_id, project_id, model_name):
+    def get(self):
+        data = request.get_json()
+        user_id = data.get('user_id')
+        project_id = data.get('project_id')
+        model_name = data.get('model_name')
         stats = stats_collection.find_one({
             'user_id': user_id,
             'project_id': project_id,
--- Training/TrainingTest.py
+++ Training/TrainingTest.py
@@ -10,7 +10,7 @@ from Training import app, StartTraining, GetTrainingStats
 logging.basicConfig(filename='./testTraining.log', filemode='a', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 mylogger = logging.getLogger()
-fhandler = logging.FileHandler(filename='testDataUpload.log', mode='a')
+fhandler = logging.FileHandler(filename='testTraining.log', mode='a')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fhandler.setFormatter(formatter)
 mylogger.addHandler(fhandler)
@@ -26,32 +26,43 @@ def client():
 
 
 def test_upload_parameters(client):
-    datasets = load_dataset("food101", split="train[:5000]")
-    datasets.save_to_disk('/home/thai/training_test/')
+    test_set = load_dataset("food101", split="train[:100]")
+    #test_set.save_to_disk('./training_test')
+    test_set.to_parquet('./training_test/training_test.parquet')
     rv = client.post('/upload_parameters', json={
         'user_id': 'test_user',
         'project_id': 'test_project',
-        'parameters': {'num_train_epochs': 3, 'per_device_train_batch_size': 16, 'per_device_eval_batch_size': 64,
-                       'warmup_steps': 500, 'weight_decay': 0.01},
-        'train_dataset': '/home/thai/training_test/'
+        'parameters': {
+                        'learning_rate': 5e-5,
+                        'per_device_train_batch_size':16,
+                        'gradient_accumulation_steps':4,
+                        'per_device_eval_batch_size':16,
+                        'num_train_epochs': 3,
+                        'warmup_ratio':0.1,
+                        'logging_steps': 10,
+    },
+        'train_dataset': './training_test/training_test.parquet'
     })
     assert b'Parameters uploaded successfully' in rv.data
 
 
 
 def test_start_training(client):
-
-
     rv = client.post('/start_training', json={
         'user_id': 'test_user',
         'project_id': 'test_project',
         'model_name': 'google/vit-base-patch16-224-in21k'
     })
-    assert b'Training for model test_model completed successfully' in rv.data
+    assert rv.status_code ==200
+   # assert b'Training for model test_model completed successfully' in rv.data
 
 
 def test_get_training_stats(client):
-    rv = client.get('/get_training_stats/test_user/test_project/test_model')
+    rv = client.get('/get_training_stats',json = {
+        'user_id': 'test_user',
+        'project_id': 'test_project',
+        'model_name': 'google/vit-base-patch16-224-in21k'
+    })
     assert rv.status_code == 200
     current, peak = tracemalloc.get_traced_memory()
     mylogger.info(f"Current memory usage is {current / 10 ** 6}MB; Peak was {peak / 10 ** 6}MB")
